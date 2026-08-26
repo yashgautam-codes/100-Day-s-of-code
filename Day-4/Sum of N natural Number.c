11b43f66e9c5a1bc99aefd4d97f4fca938728673
yashgautam-codes <yashgautam8368@gmail.com>
2026-08-26 12:49:29 +0530

    Add program to compute sum of first N natural numbers
    Implement a program to calculate the sum of the first 'N' natural numbers.

---
+++ Day-4/Sum of N natural Number.c
@@ -0,0 +1,11 @@
+// Q8. Write  a program to find & display sum of first 'N' natural number's. 
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number 'N' till which sum need's to be printed :- ");
+    scanf("%d",&n);
+    int q = n*(n+1)/2;   //sum of first 'N' natural number's
+    printf("Sum of the first 'N' natural number's :- %d",q);
+    return 0;
+}
