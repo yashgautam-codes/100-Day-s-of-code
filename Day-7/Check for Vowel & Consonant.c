d63307879df791c6b382aa7f02b1a7b847c54535
yashgautam-codes <yashgautam8368@gmail.com>
2026-08-26 12:58:36 +0530

    Add program to check for vowel or consonant

---
+++ Day-7/Check for Vowel & Consonant.c
@@ -0,0 +1,17 @@
+// Q14. Write a program to input a character and check whether it is a vowel or consonent using if-else.
+#include <stdio.h>
+int main()
+{
+    char n;      // Character should not be capital.
+    printf("Enter Any Character:- ");
+    scanf("%c",&n);
+    if( n =='a' || n =='e' || n =='i' || n =='o' || n =='u')
+    {
+        printf("Vowel!");
+    }
+    else
+    {
+        printf("Consonent!");
+    }
+    return 0;
+}
