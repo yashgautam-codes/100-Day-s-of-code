3b08857dae6df19ffd2d7c90e1a140e44eafc30f
yashgautam-codes <yashgautam8368@gmail.com>
2026-08-26 12:46:34 +0530

    Create Conversion of Celcius to Fahrenheit.c

---
+++ Day-3/Conversion of Celcius to Fahrenheit.c
@@ -0,0 +1,11 @@
+// Q5. Write a program to convert temperature from Celcius to Fharenheit.
+#include <stdio.h>
+int main()
+{
+    float c;    // c->Celcius
+    printf("Enter the temperature in Celcius :- ");
+    scanf("%f",&c);
+    float f = (c*1.8)+32;  // f->Fahrenheit
+    printf("Temperature in Fahrenheit :- %f",f);
+    return 0;
+}
