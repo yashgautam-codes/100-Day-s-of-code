dc298d23bc6763fdf0c60666124878938c4ff0ac
yashgautam-codes <yashgautam8368@gmail.com>
2026-08-26 12:59:30 +0530

    Add leap year checking program in C
    Implement a program to check for leap years based on user input.

---
+++ Day-7/Check for LeapYear.c
@@ -0,0 +1,17 @@
+// Q13. Write a program to input a year and check whether it is a leap year or not using conditional statements.
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter any Year:- ");
+    scanf("%d",&n);
+    if(n%4==0)
+    {
+        printf("LEAP YEAR!");
+    }
+    else
+    {
+        printf("NOT A LEAP YEAR");
+    }
+    return 0;
+}
