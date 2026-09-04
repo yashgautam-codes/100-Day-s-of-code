4ebffc070622775f0b723b0e6d06a176c1404321
yashgautam-codes <yashgautam8368@gmail.com>
2026-09-04 11:35:57 +0530

    Add C program for star pattern printing
    Implemented a C program to print a star pattern.

---
+++ Day-24/Pattern2.c
@@ -0,0 +1,19 @@
+// Q47. Write a program to print the following pattern:
+// *
+// **
+// ***
+// ****
+// *****
+#include<stdio.h>
+int main()
+{
+  for(int i=0;i<=4;i++)
+    {
+      for(int j=0;j<=i;j++)
+        {
+          printf("*");
+        }
+      printf("\n");
+    }
+  return 0;
+}
