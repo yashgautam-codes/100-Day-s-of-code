ad03352048b654675a237433976dbd525e2d4dc2
yashgautam-codes <yashgautam8368@gmail.com>
2026-09-04 11:33:07 +0530

    Correct pattern printing in Pattern1.c
    Fix syntax errors and improve pattern printing logic.

---
+++ Day-23/Pattern1 .c
@@ -0,0 +1,18 @@
+// Q46. Write a program to print the following pattern:
+// *****
+// *****
+// *****
+// *****
+// *****
+#inclue <stdio.h>
+int main()
+{
+  for(int i=0;i<=4;i++)
+  {
+    for(int j=0;j<=4;j++)
+      {
+        printf("*");
+      }
+    printf("\n);
+    return 0;
+}
