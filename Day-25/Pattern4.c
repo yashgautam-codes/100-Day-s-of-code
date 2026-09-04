603aacffdede4e0476fbda5f49f2dd779ef01658
yashgautam-codes <yashgautam8368@gmail.com>
2026-09-04 11:44:30 +0530

    Add pattern printing program in Pattern4.c
    Implemented a program to print a specific number pattern.

---
+++ Day-25/Pattern4.c
@@ -0,0 +1,19 @@
+// Q49. Write a program to print the following pattern:
+// 5
+// 45
+// 345
+// 2345
+// 12345
+#include <stdio.h>
+int main()
+{
+  for(int i=5;i>=1;i--)
+    {
+      for(intj=i;j<=5;j++)
+        {
+          printf("%d",j);
+        }
+      printf("\n");
+    }
+  return 0;
+}
