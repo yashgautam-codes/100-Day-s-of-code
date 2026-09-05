c4657f9781820552d218dfab7cb66cd4e45c2872
yashgautam-codes <yashgautam8368@gmail.com>
2026-09-05 09:46:17 +0530

    Create Pattern8.c

---
+++ Day-27/Pattern8.c
@@ -0,0 +1,31 @@
+// Q53. Write a program to print the following pattern:
+// *
+// ***
+// *****
+// *******
+// *********
+// *******
+// *****
+// ***
+// *
+#include <stdio.h>
+int main()
+{
+  for(int i=1;i<=5;i++)
+  {
+    for(int j=1;j<=2*i-1;j++)
+    {
+      printf("*");
+    }
+    printf("\n");
+  }
+  for(int i=4;i>=1;i--)
+  {
+    for(int j=1;j<=2*i-1;j++)
+    {
+      printf("*");
+    }
+    printf("\n");
+  }
+  return 0;
+}
